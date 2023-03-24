137dabaf3835b3908b416f8f31484363c67f2784
Ankan Hazra <ankanhazraeducation@gmail.com>
2023-03-24 12:29:33 +0530

    Add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Header.css';
+
+function Header(){
+    return (
+      <div className="header">
+          <h1>Resumen_</h1>
+          <p>Online Resume Builder</p>
+      </div>
+    );
+}
+
+export default Header;
